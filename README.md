0adc9030feff202b7fa85347ec5d0a6aa480dd01
yare-c <123@gmail.com>
2023-03-04 18:38:52 +0300

    latest updates

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # wine-dataset
 For everyone, who love wine!
+A simple programm, which can provide simple statistics about wine quality. 
+All code has a commentaries and written in an understandable syntax.
+
+# The Algorithm of working with programm
+1. Choose two parameters of wine and type number of this parameter to x axis first and then for y axis
+2. Just enjoy and analyse the data
+3. Thank you Lacalute :)
+
+# The Link with dataset
+https://archive.ics.uci.edu/ml/datasets/Wine
--- q.py
+++ q.py
@@ -1,74 +1,67 @@
+# import neccessary libraries
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
+# open csv file
 with open('winequality-red.csv') as f:
     reader = csv.reader(f)
-
     x = list(reader)
-    x.pop(0)
 
-y = []
+# the creating new big array with data, without 3D structure
+d = []
 i = 0 
 while i < len(x):
     x[i][0] = x[i][0].split(";")
-    y.append(x[i][0])
+    d.append(x[i][0])
     i = i + 1
 
+titles = d[0]
+d.pop(0)
 
+# indexes
+# let's imagine 1 is 0 (index I meant)
+# 1 - fixed acidity
+# 2 - volatile acidity
+# 3 - citric acid
+# 4 - residual sugar
+# 5 - chlorides
+# 6 - free sulfur dioxide
+# 7 - total sulfur dioxide
+# 8 - density
+# 9 - pH
+# 10 - sulphates
+# 11 - alcohol
+# 12 - quality (score between 0 and 10)
 
+# the main func, which collect data by index and visualize them
+status = True
+print("0 - fixed acidity\n1 - volatile acidity\n2 - citric acid\n3 - residual sugar\n4 - chlorides\n5 - free sulfur dioxide\n6 - total sulfur dioxide\n7 - density\n8 - pH\n9 - sulphates\n10 - alcohol\n11 - quality (score between 0 and 10)")
+while status:
+    try:
+        ask_x = int(input('Choose the data on the x axis -> '))
+        ask_y = int(input('Choose the data on the y axis -> '))
+        def program(index_x, index_y):
+            x_set = []
+            y_set = []
+            z = 0 
+            while z < len(d):
+                x_set.append(float(d[z][index_x]))
+                y_set.append(float(d[z][index_y]))
+                z = z + 1
+            # on this step we've got the data for plotting
 
-ph = []
-qualities = []
-z = 0 
-while z < len(y):
-    ph.append(y[z][4])
-    qualities.append(y[z][11])
-    z = z + 1
+            def plotting():
+                plt.style.use('ggplot')
+                plt.scatter(x_set, y_set)
+                plt.title(f'Compare {titles[index_x]} with {titles[index_y]}')
+                plt.xlabel(titles[index_x])
+                plt.ylabel(titles[index_y])
+                plt.show()
+            plotting()
+        program(ask_x, ask_y)
+        status = False
 
-
-
-ph_data = [float(q) for q in ph]
-
-
-
-ger = {3: [], 4: [], 5: [], 6: [], 7: [], 8: []}
-lis = list(ger.keys())
-
-qu = [int(po) for po in qualities]
-
-def sort():
-    counter = 0
-    while counter < len(lis):
-        af = 0
-        while af < len(ph_data):
-            if qu[af] == lis[counter]:
-                ger[lis[counter]].append(ph_data[af])
-                af = af + 1
-            else:
-                af = af + 1
-        counter = counter + 1
-    
-    
-sort()
-
-
-
-ph_finale = []
-h = 0
-while h < len(lis):
-    ph_finale.append(sum(ger[lis[h]]) / len(ger[lis[h]]))
-    h = h + 1
-
-
-print(ph_finale)
-
-def plotting():
-    plt.style.use('ggplot')
-    plt.xlabel('Качество (от 1 до 10)')
-    plt.ylabel('Хлориды')
-    plt.title('Красное вино')
-    plt.bar(lis, ph_finale,label=lis)
-    plt.show()
-
-plotting()
+    except ValueError:
+        print('Wrong data, again please')
+# simple statistic programm by Lacalute !!!
